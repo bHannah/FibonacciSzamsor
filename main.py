02ae4c9e2a4009ae22d76d8fded2b32e52606cf4
bHannah <borsikaaa1@gmail.com>
2024-11-19 08:02:46 +0100

    fibonacci számsor x elemének kiszámolása python segítségével, konzolos felület

---
+++ main.py
@@ -0,0 +1,12 @@
+menny = int(input("Hány számot számítsunk ki?: "))
+
+lista = []
+elso_elem = 0
+masodik_elem =1
+print("A fibonacci sorozat első két eleme: ", elso_elem, " ", masodik_elem)
+for i in range(menny):
+  szamitott = elso_elem+masodik_elem
+  print(szamitott)
+  elso_elem = masodik_elem
+  masodik_elem = szamitott
+print("Megállapítható hogy a számsorozat mindig az előző két szám összege!")
